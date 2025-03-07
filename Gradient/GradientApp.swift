a9152d961af760d6e1f8861f152e02fe7685a789
maxpurdon <maxpurdonwork@gmail.com>
2025-03-07 17:53:32 +0100

    Auto Commit from GUI

--- Gradient/GradientApp.swift
+++ Gradient/GradientApp.swift
@@ -4,17 +4,3 @@
 //
 //  Created by Andrew Purdon on 06/03/2025.
 //
-
-import SwiftUI
-
-@main
-struct GradientApp: App {
-    let persistenceController = PersistenceController.shared
-
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
-        }
-    }
-}
--- Gradient/Utils/AppDelegate.swift
+++ Gradient/Utils/AppDelegate.swift
@@ -10,7 +10,9 @@ import UIKit
 import Firebase
 import UserNotifications
 
+@main
 class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+    var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Configure Firebase
@@ -19,9 +21,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         // Configure notifications
         UNUserNotificationCenter.current().delegate = self
         
+        // Set up window and root view controller
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let projectListVC = ProjectListViewController()
+        let navigationController = UINavigationController(rootViewController: projectListVC)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+        
         return true
     }
     
+    // Remove or comment out the scene methods if you're not using scenes
+    /*
     // MARK: - UISceneSession Lifecycle
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
@@ -30,7 +41,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
     }
+    */
     
+    // Rest of the methods...
+}
     // MARK: - UNUserNotificationCenterDelegate
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
         // Show notification even when app is in foreground
@@ -42,4 +56,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         // In a real app, we would navigate to the relevant task
         completionHandler()
     }
-}
